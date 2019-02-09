5b96140cbc75aa86edd041ad7eb3e15b3d47d1d6
Alex Art <alex.art@in2circle.com>
2019-02-09 19:13:43 +0400

    * fix writing to wrong memory error related to SIZE_OF_TEMPERATURES_BUFFER

--- src/Thermostat.cpp
+++ src/Thermostat.cpp
@@ -3,13 +3,14 @@
 
 const float MINIMAL_DESIRED_TEMPERATURE = 10.0;
 const float MAXIMAL_DESIRED_TEMPERATURE = 40.0;
-const uint8_t SIZE_OF_TEMPERATURES_BUFFER = 5;
 
 enum Mode : boolean {
     AUTO, MANUAL
 };
 
 class TemperatureStack {  // NOLINT(cppcoreguidelines-pro-type-member-init,hicpp-member-init)
+public:
+    static const uint8_t SIZE_OF_TEMPERATURES_BUFFER = 5;
 private:
     float temperatures[SIZE_OF_TEMPERATURES_BUFFER];
     uint8_t lastPointerPosition = 0;
@@ -24,15 +25,14 @@ public:
     void addTemperature(float temperature) {
         if (isnan(temperature) || temperature < 0 || temperature > 100)
             return;
-        if (lastPointerPosition >= 10)
+        if (lastPointerPosition++ >= SIZE_OF_TEMPERATURES_BUFFER)
             lastPointerPosition = 0;
         temperatures[lastPointerPosition] = temperature;
-        lastPointerPosition++;
     }
 
     float getAverageTemperature() {
         float sum = 0;
-        for (float temperature : temperatures) {
+        for (float &temperature : temperatures) {
             sum += temperature;
         }
         return sum / SIZE_OF_TEMPERATURES_BUFFER;
